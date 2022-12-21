08fe2a036d88ea66328e108edd11f6322d446026
emotzart <johnshuabratsk@gmail.com>
2022-12-21 16:06:46 +0700

    refactor User Model, docker-compose file downgrade simple-jwt version for compatible with djoser

--- users/models.py
+++ users/models.py
@@ -2,20 +2,52 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from users.manager import UserManager
 
 
 class User(AbstractUser):
+    objects = UserManager()
+
+    #roles
     USER = 'user'
     ADMIN = 'admin'
     ROLES = [
         (USER, 'пользователь'),
-        (ADMIN, 'администратор')
+        (ADMIN, 'администратор'),
     ]
 
+    #fields
     phone = models.CharField(max_length=14, unique=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLES, default=USER)
     image = models.ImageField()
+    username = models.CharField(null=True, blank=True, max_length=150)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', "role"]
+    #
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == self.USER
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
 
     class Meta:
         ordering = ['username']
