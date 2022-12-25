2651fac0cc0e79150a7c77a2d6e95a3dc94f8664
emotzart <johnshuabratsk@gmail.com>
2022-12-25 23:17:50 +0700

    Ad and comment models refactoring

--- ads/models.py
+++ ads/models.py
@@ -4,9 +4,8 @@ from django.db import models
 
 
 class Ad(models.Model):
-
-    title = models.CharField(max_length=200, null=False)
-    price = models.PositiveIntegerField()
+    title = models.CharField(max_length=200, null=False, blank=False)
+    price = models.PositiveIntegerField(null=False, blank=False)
     description = models.CharField(max_length=1000, null=True)
     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,10 +20,10 @@ class Ad(models.Model):
 
 
     def __str__(self):
-        return f"{self.id}: {self.name} - {self.price}"
+        return f"{self.id}: {self.title} - {self.price}"
 
 class Comment(models.Model):
-    text = models.CharField(max_length=1000, null=False)
+    text = models.CharField(max_length=1000, null=False, blank=False )
     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
     ad = models.ForeignKey('ads.Ad', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
